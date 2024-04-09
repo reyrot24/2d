7c6861d70a7202e2f501d59c21a7ce6860f771a6
reyrot24 <reyrotaru1@gmail.com>
2024-04-09 21:10:52 +0200

    first-commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/8-bit-project/",
+  base: "/2d-project/",
   plugins: [react()],
 });
